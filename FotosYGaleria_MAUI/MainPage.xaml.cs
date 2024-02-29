57ae11f5449935b5c9e7ef4fa1c97861678374be
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2024-02-29 11:30:42 -0700

    subir

---
+++ FotosYGaleria_MAUI/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace FotosYGaleria_MAUI;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+
+    private async void MostrarGaleria(object sender, EventArgs e)
+    {
+        var foto = await MediaPicker.PickPhotoAsync();
+
+        if (foto != null)
+        {
+            var memoryStraeam = await foto.OpenReadAsync();
+            imgFoto.Source = ImageSource.FromStream(() => memoryStraeam);
+        }
+    }
+
+    private async void TomarFoto(object sender, EventArgs e)
+    {
+        var foto = await MediaPicker.CapturePhotoAsync();
+
+        if (foto != null)
+        {
+            var memoryStraeam = await foto.OpenReadAsync();
+            imgFoto.Source = ImageSource.FromStream(() => memoryStraeam);
+        }
+    }
+}
+
